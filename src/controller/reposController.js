71dc7d8146c4e2ca4cf1bec1eee66e7c108253f4
Junior apostolo <junior-apostolo>
2022-06-30 21:49:43 -0300

    fix: ajustes na service

--- src/controller/reposController.js
+++ src/controller/reposController.js
@@ -5,10 +5,11 @@ module.exports = {
     try {
       const reposList = await apiService();
 
-      return res.json({ reposList });
+      return res.status(200).json({ reposList });
 
     } catch (error) {
       console.log(error)
+      return res.status(500).json({ error: "Erro interno" })
     }
 
   }
